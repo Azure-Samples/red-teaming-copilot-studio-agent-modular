8ac4383b36f8d46e05222847e455aba258fd9396
Cheng Chen <chencheng@microsoft.com>
2025-12-16 12:51:23 +0800

    init

---
+++ targets/__init__.py
@@ -0,0 +1,5 @@
+"""Target implementations for red team evaluation."""
+
+from .mcs_agent_callback import McsAgentCallbackTarget, McsAgentConfig
+
+__all__ = ["McsAgentCallbackTarget", "McsAgentConfig"]
